48ae4cb537beb18ce4812cecedbf6e2a42d11daa
Piotr Jamroz <pm.jamroz@gmail.com>
2017-08-21 21:56:10 +0100

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,9 @@ module.exports = {
         index: './index.js'
     },
     output: {
-        filename: 'aw-parser.js',
+        filename: 'aw-parser.amd.js',
         libraryTarget: "umd",
+        library: 'awParser',
         path: path.resolve(__dirname, 'dist')
     }
 };
